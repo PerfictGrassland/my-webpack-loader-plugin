c84ca8060a1d9a5d60e8f67e9db95f2c031a1f7c
Chen, James SJ <82010157@corphq.hk.pccw.com>
2024-09-18 14:16:28 +0800

    更新viewport配置

--- utils/mobileConfig/index.js
+++ utils/mobileConfig/index.js
@@ -4,13 +4,13 @@ export const setViewport = function (scale) {
   if (viewportMeta) {
     viewportMeta.setAttribute(
       "content",
-      `width=device-width, initial-scale=${scale}`
+      `width=device-width, initial-scale=${scale}, maximum-scale=${scale}, minimum-scale=${scale}, user-scalabel=no, shrink-tofit=no`
     );
   } else {
     // 如果没有找到，则创建新的 meta 标签
     viewportMeta = document.createElement("meta");
     viewportMeta.name = "viewport";
-    viewportMeta.content = `width=device-width, initial-scale=${scale}`;
+    viewportMeta.content = `width=device-width, initial-scale=${scale}, maximum-scale=${scale}, minimum-scale=${scale}, user-scalabel=no, shrink-tofit=no`;
     document.head.appendChild(viewportMeta);
   }
 };
